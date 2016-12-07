956f167472fc887681bb8c0101b7558d9ca98c7b
SevaTsurikov <seva.tsurikov@tweglobal.com>
2016-12-07 14:50:43 -0800

    version 2

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Update: 7/24/2015
 -----------------
 User input veryfication is added to the full version. The.error class is added if date/time is entered incorrect. 
 Time/date builder is added on the plugin page: http://codegen.in/timeit/#build
+
+Update: 12/07/2016
+-----------------
+- code refactored: less functions, more comments
+- the will loop through all the timestamps and define if an update will be needed. If so, the update will be delayed. The minimal time frame is 1 minute.
+Example: A user opens a page 5 minutes till a content will become (un)avaialbe. The page will be updated without reloading on the exact time.
--- timeit.js
+++ timeit.js
@@ -1,71 +1,80 @@
-$(function() {
+function timeit() {
+  var next_run_array = []; //array of dates/time on a page used to rerun function if a change should happen during the session
+  var curDate = new Date();
+  Date.prototype.yyyymmdd = function() {
+    var mm = this.getMonth() + 1;
+    var dd = this.getDate();
+    return [this.getFullYear(),
+      (mm > 9 ? '' : '0') + mm,
+      (dd > 9 ? '' : '0') + dd
+    ].join('-');
+  };
+  var curDateYMD = curDate.yyyymmdd();
+
   $('.timeit').each(function() {
+    var end = $(this).data('end'),
+      start = $(this).data('start');
+    //check if date or time value has valid format and push it to the list of refresh anchors
+    var startDate = checkdate(start, this);
+    var endDate = checkdate(end, this);
+    nextrun(startDate);
+    nextrun(endDate);
+    
+	//add a datetime when the page needs to be refreshed (now+24 hrs time span only)
+    function nextrun(date) {
+      var nextruntimeout = date - curDate;
+      if (nextruntimeout < 1000 * 60 * 60 * 24 && nextruntimeout > 1000) {
+        next_run_array.push(nextruntimeout);    
+      }
+    }
 
-    //check if date or time value has valid format
-    if ($(this).data('end')) {
-      checkdate('end', this);
-    };
-    if ($(this).data('start')) {
-      checkdate('start', this);
-    };
-
-    //check if the evend outside of desired timespan
-    var curDate = new Date();
-    var startDate = roundDate(oTime($(this).data('start')));
-    var endDate = roundDate(oTime($(this).data('end')));
+    // Main Function 
+    //check if the evend outside of a desired time span
     if (((startDate < endDate) && (startDate > curDate || endDate < curDate)) ||
       ((startDate > endDate) && (startDate >= curDate) && (endDate <= curDate))
     ) {
       $(this).addClass('hidden');
-    };
+    } else {
+      $(this).removeClass('hidden');
+    }
 
-    function checkdate(d, t) {
-      //check if the value is valid time (not a date)
-      var time = $(t).data(d);      
-      var res = time.split(":");
-      var h = res.slice(0, 1),
-        m = res.slice(1),
-        re = /\d:\d/g,
-        tt = re.test(time);
-      if (h < 25 && m < 61 && tt) {
-        var timetest = true;
-      }
-
-      //check if date is valid (avoid valid time)
-      var cd = new Date($(t).data(d));
-      if (cd == 'Invalid Date' && !timetest) {
-       //highlight the element if the is an error. use own .error class if needed
-        $(t).addClass("error").attr("title", d + " date is incorrect; please use YYYY-MM-DD HH:MM format");
-      };
-    };
-
-    //round time to avoid errors 
-    function roundDate(t) {
-      if (Math.abs(curDate.getTime() - t) < 10) {
-        var t = curDate.getTime();
-      };
-      return t;
-    };
-
-    function oTime(t) {
-      t = t || 'invalid date';
-      if (t == 'invalid date') {
-        var oDate = new Date();
-      } else {
-        var oDate = new Date(t);
-        if (!Date.parse(oDate)) {
-          var oDate = new Date();
+	//Support Functions
+    function checkdate(date, obj) {
+      if (date) {
+        //check if only time is set; if so, add today's date to unify dates format
+        if (String(date).length < 6 && String(date).indexOf(":") > -1) {
+          date = curDateYMD + ' ' + String(date);
         }
-        oDate.setHours(
-          parseInt(t.substr(0, 2), 10),
-          parseInt(t.substr(3, 2), 10),
-          0,
-          0
-        );
-      };
-      var callback = oDate.getTime();
-      return callback;
-    };
+        //check if only date is set; if so add 00:00 to the end of date
+        if (String(date).indexOf(":") == -1) {
+          date = date + ' 00:00';
+        }
+        //check if date is valid (avoid valid time)
+        var res = date.split(":"),
+          h = String(res.slice(0, 1)),
+          hours = h.substr(h.length - 2),
+          minutes = res.slice(1);
+        var timetest = (hours < 24 && minutes < 60) ? true : false;
+
+        if (new Date(date) == 'Invalid Date' || !timetest) {
+          //highlight the element if the is an error. use own \.error class if needed
+          $(obj).addClass("error").attr('title', '"' + date + '" date is incorrect; please use YYYY-MM-DD HH:MM format');
+        }
+        return new Date(date).getTime();
+      } else {
+        //if datetime is not set, just return current date-time
+        return curDate.getTime();
+      }
+    }
   });
 
-});
+  /* Schedule next runs */
+  if (next_run_array.length > 0) {  
+    var nextruntime = Math.min.apply(null, next_run_array);
+    console.log("next run of timeit function is in " +  nextruntime / 1000 + "seconds");
+    setTimeout(function() {
+      timeit();
+    }, nextruntime);
+  }
+}
+timeit();
